a61cde72cecd4630b7df1df3544a5314330da40d
Conor <cjhfitz@gmail.com>
2020-07-29 17:39:51 -0400

    added delete todo functionality

--- assets/js/todos.js
+++ assets/js/todos.js
@@ -2,3 +2,11 @@
 $("li").click(function() {
     $(this).toggleClass("completed");
 });
+
+// click on X to delete todo
+$("span").click(function(event) {
+    $(this).parent().fadeOut("500ms", function() {
+        $(this).remove();
+    });
+    event.stopPropagation();
+});
