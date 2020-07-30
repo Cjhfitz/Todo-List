56571e9adfe15b519d45d7c2bb205bfb239fced0
Conor <cjhfitz@gmail.com>
2020-07-29 21:52:39 -0400

    added more functionality

--- assets/js/todos.js
+++ assets/js/todos.js
@@ -1,12 +1,21 @@
 // Check off specific todo by clicking
-$("li").click(function() {
+$("ul").on("click", "li", function() {
     $(this).toggleClass("completed");
 });
 
 // click on X to delete todo
-$("span").click(function(event) {
+$("ul").on("click", "span", function(event) {
     $(this).parent().fadeOut("500ms", function() {
         $(this).remove();
     });
     event.stopPropagation();
+});
+
+// 
+$("input[type='text']").on("keypress", function(event) {
+    if(event.which === 13) {
+        let todoText = $(this).val();
+        $(this).val("");
+        $("ul").append("<li><span>X </span>"+ todoText + "</li>");
+    }
 });
